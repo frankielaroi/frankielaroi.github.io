f46e5390bdf85dff17b870f7365d8fe6ceac3013
frankielaroi <frankiekweku@gmail.com>
2024-05-23 23:41:36 +0100

    "Updated index.html, script.js, and style.css files with significant changes to layout, design, and functionality."

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let Header =getDocument.by
+// script.js
+function toggleDrawer() {
+  const drawer = document.querySelector('.drawer');
+  if (drawer.classList.contains('drawer-open')) {
+    drawer.classList.remove('drawer-open');
+  } else {
+    drawer.classList.add('drawer-open');
+  }
+}
